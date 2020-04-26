54f8455c867609d67d259535b04fccb284c4a413
zhouwei <972610556@qq.com>
2020-04-26 14:23:48 +0800

    Update xray.py

--- xmind2testlink/xray.py
+++ xmind2testlink/xray.py
@@ -9,11 +9,16 @@ from xmind2testlink.datatype import TestCase
 
 class xray_issue:
 
-    def create_xray_issue(project_name_key, issue_name, jira_token):
+    def create_xray_issue(project_name_key, issue_name, jira_token, importance):
         url = "https://olapio.atlassian.net/rest/api/2/issue"
-
+        importance_list = [0, 1, 2, 3]
+        if int(importance) not in importance_list:
+            importance = 3
+        issue_name = str(issue_name).replace('\r\n', '')
         payload = {
-            "fields": {"project": {"key": project_name_key}, "summary": issue_name,
+            "fields": {"project": {"key": project_name_key},
+                       "summary": issue_name,
+                       "priority": {"name": "P" + str(importance)},
                        "description": "example of manual test",
                        "issuetype": {"name": "Test"}}}
         headers = {
@@ -37,10 +42,15 @@ class xray_issue:
 
         response = requests.post('https://xray.cloud.xpand-it.com/api/internal/test/' + key + '/step', headers=headers,
                                  data=json.dumps(data))
+        if response.status_code == 500:
+            print(response.json()['error'])
+            exit(1)
+        # else:
+        #     print('创建步骤成功')
 
     def create_xray_full_issue(project_name_key, issue_name, test_case, link_issue_key, jira_token, X_acpt):
         # test_case = TestCase(test_case)
-        (id, key) = xray_issue.create_xray_issue(project_name_key, issue_name, jira_token)
+        (id, key) = xray_issue.create_xray_issue(project_name_key, issue_name, jira_token, test_case.importance)
         xray_issue.link_issue(link_issue_key, key, jira_token)
 
         for i in range(len(test_case.steps)):
@@ -78,12 +88,12 @@ class xray_issue:
 
 # create_xray_full_issue()
 if __name__ == '__main__':
-    X_acpt = ''
-    jira_token = 'XWGNZ4MgoeD1kfofTelQ72CD'
+    # X_acpt = ''
+    jira_token = 'd2VpLnpob3VAa3lsaWdlbmNlLmlvOm8xeGh0M2owSVdheUdxWWx4bUUwNzU2Rg=='
     xray_issue = xray_issue()
-    # xray_issue.get_issue_info()
-    project_name_key = 'QUARD'
-    issue_name = 'test_issue'
-    test_case = ''
-    link_issue_key = ''
-    xray_issue.create_xray_full_issue(project_name_key, issue_name, test_case, link_issue_key)
+    xray_issue.get_issue_info()
+    # project_name_key = 'QUARD'
+    # issue_name = 'test_issue'
+    # test_case = ''
+    # link_issue_key = ''
+    # xray_issue.create_xray_full_issue(project_name_key, issue_name, test_case, link_issue_key)
